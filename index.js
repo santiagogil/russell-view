5bcb2ecd2d9d4b00d90438f45c13ed025fc18171
Santiago Gil <gil.educaciontdf@gmail.com>
2016-06-16 15:32:01 -0300

    fix: Add credit to original module

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Inspired on es6-template-tags
 module.exports = function russellView (literals, ...substs) {
   return literals.raw.reduce(function reducelit (acc, lit, i) {
     var subst = substs[i - 1]
